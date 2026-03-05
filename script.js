11d63a7baea949b8bd8f368af93390f3e54f9b9a
Prasanna-manepalli <prasannamanepalli69@gmail.com>
2026-03-05 13:58:10 +0530

    Updated portfolio

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 function toggleExpand(card){
   card.classList.toggle("active");
 }
-const hamburger = document.getElementById("hamburger");
-const navLinks = document.getElementById("navLinks");
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+const navItems = document.querySelectorAll(".nav-link");
 
+/* open/close menu */
 hamburger.addEventListener("click", () => {
   navLinks.classList.toggle("active");
+});
+
+/* close menu when clicking any link */
+navItems.forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("active");
+  });
 });
